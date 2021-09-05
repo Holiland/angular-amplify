902b4542da47267f1f15e164239edb4fb5f44a23
Holiland <120149114+Holiland@users.noreply.github.com>
2021-09-05 21:10:31 +0000

    Delete 0

--- scripts/multidiffusion.py
+++ scripts/multidiffusion.py
@@ -10,3 +10,8 @@
 #
 #   The script contains a few optimizations including:
 #       - symmetric tiling bboxes
+#       - cached tiling weights
+#       - batched denoising
+#       - prompt control for each tile (in progress)
+#
+# ------------------------------------------------------------------------
