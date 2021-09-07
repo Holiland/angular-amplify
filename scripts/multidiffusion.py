2e41dd6476f99388ea007a308e6dc3ea512463cf
Holiland <120149114+Holiland@users.noreply.github.com>
2021-09-07 17:26:51 +0000

    Use Docker at Travis

--- scripts/multidiffusion.py
+++ scripts/multidiffusion.py
@@ -22,3 +22,7 @@
 #   Advantages:
 #   - Allows for super large resolutions (2k~8k) for both txt2img and img2img.
 #   - The merged output is completely seamless without any post-processing.
+#   - Training free. No need to train a new model, and you can control the
+#       text prompt for each tile.
+#
+#   Drawbacks:
