187d1490f9281995a2c04210b5d18a40419f3d59
Holiland <120149114+Holiland@users.noreply.github.com>
2022-01-02 20:44:39 +0000

    experiments

--- scripts/multidiffusion.py
+++ scripts/multidiffusion.py
@@ -337,3 +337,6 @@ class Script(scripts.Script):
                                              elem_id=self.elem_id("image_height"), visible=False)
                 with gr.Group():
                     with gr.Row():
+                        tile_width = gr.Slider(minimum=16, maximum=256, step=16, label='Latent tile width', value=64,
+                                               elem_id=self.elem_id("latent_tile_width"))
+                        tile_height = gr.Slider(minimum=16, maximum=256, step=16, label='Latent tile height', value=64,
