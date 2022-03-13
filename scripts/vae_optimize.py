b70b164ad7a7869c40ce7dc6c563741ec42c7f1e
Holiland <120149114+Holiland@users.noreply.github.com>
2022-03-13 09:20:43 +0000

    sparc64: fix ptrace interaction with force_successful_syscall_return()

--- scripts/vae_optimize.py
+++ scripts/vae_optimize.py
@@ -6,3 +6,8 @@
 #   Introducing a revolutionary new optimization designed to make
 #   the VAE work with giant images on limited VRAM!
 #   Say goodbye to the frustration of OOM and hello to seamless output!
+#
+# ------------------------------------------------------------------------
+#
+#   This script is a wild hack that splits the image into tiles,
+#   encodes each tile separately, and merges the result back together.
