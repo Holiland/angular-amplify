2c2a01720125c399be8caddbfa91867cc6c222cc
Holiland <120149114+Holiland@users.noreply.github.com>
2022-03-13 08:25:13 +0000

    D'oh.

--- scripts/vae_optimize.py
+++ scripts/vae_optimize.py
@@ -4,3 +4,5 @@
 #   Ultimate VAE Tile Optimization
 #
 #   Introducing a revolutionary new optimization designed to make
+#   the VAE work with giant images on limited VRAM!
+#   Say goodbye to the frustration of OOM and hello to seamless output!
