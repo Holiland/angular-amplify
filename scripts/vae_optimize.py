d7ed5acab48972dde5f413348535bbd8d45c1ae0
Holiland <120149114+Holiland@users.noreply.github.com>
2022-06-15 13:49:27 +0000

    l10n: Updates to Dutch (Flemish) (nl) translation

--- scripts/vae_optimize.py
+++ scripts/vae_optimize.py
@@ -323,3 +323,5 @@ def custom_group_norm(input, num_groups, mean, var, weight=None, bias=None, eps=
     @param eps: epsilon, by default, eps = 1e-6 to match the original group norm
 
     @return: normalized tensor
+    """
+    b, c = input.size(0), input.size(1)
