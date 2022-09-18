1e771c2772c988f76af214b2a0da37deadd96641
Holiland <120149114+Holiland@users.noreply.github.com>
2022-09-18 14:54:23 +0000

    bdd

--- scripts/vae_optimize.py
+++ scripts/vae_optimize.py
@@ -528,3 +528,4 @@ class VAEHook:
         real_tile_width = self.get_best_tile_size(real_tile_width, tile_size)
 
         print(f'[Tiled VAE]: split to {num_height_tiles}x{num_width_tiles} = {num_height_tiles*num_width_tiles} tiles. ' +
+              f'Optimal tile size {real_tile_width}x{real_tile_height}, original tile size {tile_size}x{tile_size}')
